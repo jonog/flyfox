6ca0445d2d45c949654fd01775c005a754a66e5d
Jonathan Gomez <jonathanbgomez@gmail.com>
2014-09-21 18:44:20 +0800

    Log search request times

--- web.go
+++ web.go
@@ -6,11 +6,23 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 )
 
 const (
 	DEFAULT_ITEM_LIMIT = 10
+	CLR_0 = "\x1b[30;1m"
+	CLR_R = "\x1b[31;1m"
+	CLR_G = "\x1b[32;1m"
+	CLR_Y = "\x1b[33;1m"
+	CLR_B = "\x1b[34;1m"
+	CLR_M = "\x1b[35;1m"
+	CLR_C = "\x1b[36;1m"
+	CLR_W = "\x1b[37;1m"
+	CLR_N = "\x1b[0m"
 )
+
+
 
 func WebInit() {
 
@@ -32,10 +44,13 @@ func Query(res http.ResponseWriter, req *http.Request) {
 	queryParams := req.URL.Query()
 	limit := getLimit(queryParams)
 
-	items, err := Search(mux.Vars(req)["query"], limit)
+	startTime := time.Now().UTC()
+	query := mux.Vars(req)["query"]
+	items, err := Search(query, limit)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(CLR_W, "Search query: ", CLR_Y, query, CLR_R, time.Now().UTC().Sub(startTime), CLR_N)
 
 	res.Header().Set("Content-Type", "application/json")
 	res.Header().Set("Access-Control-Allow-Origin", "*")
